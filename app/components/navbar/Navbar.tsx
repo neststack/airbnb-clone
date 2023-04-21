a140d996ed21b5e8acc8f8edb28f5f3e69847a89
Nest <nsitseo@gmail.com>
2023-04-21 18:40:48 +0530

    commit lez go

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import Container from '../Container';
+import Logo from './Logo';
+import Search from './Search';
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <div
+      className="
+      fixed 
+      z-10 
+      w-full 
+      bg-white shadow-sm"
+    >
+      <div
+        className="
+        border-b-[1px] 
+        py-4"
+      >
+        <Container>
+          <div
+            className="
+              flex
+              flex-row
+              items-center
+              justify-center
+              gap-3
+              md:gap-0
+            "
+          >
+            <Logo />
+            <Search />
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,21 @@
+const Search = () => {
+  return (
+    <div
+      className="
+      w-full
+      cursor-pointer
+      rounded-full
+      border-[1px]
+      py-2
+      shadow-sm
+      transition
+      hover:shadow-md 
+      md:w-auto    
+  "
+    >
+      Search
+    </div>
+  );
+};
+
+export default Search;
